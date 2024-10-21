f2c0ffbafd06add7bdc8e84a88bb281970407897
GamerPablito <leonpablo1998@gmail.com>
2024-10-21 15:07:25 -0600

    Progress Check 2

--- script.js
+++ script.js
@@ -1,12 +1,11 @@
 var head = document.querySelector('head');
 head.innerHTML += '\n<meta charset="UTF-8">\n';
-head.innerHTML += '<meta name="viewport" content="width=device-width, initial-scale=1.0">\n';
 head.innerHTML += '<link rel="shortcut icon" href="icon.png" type="image/png">\n';
 head.innerHTML += '<link rel="stylesheet" href="style.css"></link>';
 
 var nav = document.querySelector('nav');
 nav.innerHTML += '<img src="logo_funkyversal.png" alt="Funkyversal">\n';
-nav.innerHTML += '<img src="logo_lc380.png" alt="L-C 380">\n';
+nav.innerHTML += `<img src="logo_lc380.png" alt="L-C 380" title="Go Follow L-C 380 Here!" onclick='window.open("https://twitter.com/lord_380", "_blank")'>\n`;
 
 var pbar = document.createElement('object');
 pbar.data = "progress_bar.svg";
