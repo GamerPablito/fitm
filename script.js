0cfda69ae303dce98a9a7e3a0c89ae228352b2af
GamerPablito <leonpablo1998@gmail.com>
2024-10-22 00:45:07 -0600

    Website Finished!

--- script.js
+++ script.js
@@ -12,4 +12,15 @@ pbar.data = "progress_bar.svg";
 pbar.type = "image/svg+xml";
 nav.appendChild(pbar);
 
-document.querySelector('footer').innerHTML = "Website version: v2024.10.21<br>Copyright &copy; 2024 Funkin In The Multiverse";
+var footer = document.querySelector('footer');
+footer.innerHTML += '<br><br><h2>Be Part Of The Multiverse!</h2>\n';
+footer.innerHTML += `<img id="media2" src="icon_twitter.png" alt="Twitter Logo" onclick='window.open("https://twitter.com/FunkyVersal_st", "_blank")'>\n`;
+footer.innerHTML += `<img id="media2" src="icon_youtube.png" alt="Youtube Logo" onclick='window.open("https://www.youtube.com/@funkyversalstudios", "_blank")'>\n`;
+footer.innerHTML += `<img id="media2" src="icon_gamebanana.png" alt="GameBanana Logo" onclick='window.open("https://gamebanana.com/wips/73595", "_blank")'>\n`;
+footer.innerHTML += `<img id="media2" src="icon_discord.png" alt="Discord Logo" onclick='window.open("https://discord.gg/M47FnA8VMB", "_blank")'><br><br>\n`;
+
+var p = document.createElement("p");
+p.innerText += "FNF: Funkin' In The Multiverse, created and directed by L-C 380. Developed by FunkyVersal Studio created in 2023. Gamebanana page submitted by ";
+p.innerText += "Thomas Peerman. Funkin' In The Multiverse website created by GamerPablito. Other logos and copyrights are property of their respective owners.";
+p.innerText += "Friday Night Funkin' game by Funkin' Crew Inc.\n\nAmong Us created by Innersloth, 2018\nGeometry Dash created by RobTop Games, 2013\nPlants vs Zombies created by PopCap Games, 2009";
+footer.appendChild(p);
